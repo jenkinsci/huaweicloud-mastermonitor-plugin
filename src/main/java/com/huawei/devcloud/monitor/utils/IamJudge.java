e1371349780811d1dc9ccb3f269a9e7462b8631b
liucc52 <liuchangcheng@huawei.com>
2018-10-15 09:59:58 +0800

    Update IamJudge.java

--- src/main/java/com/huawei/devcloud/monitor/utils/IamJudge.java
+++ src/main/java/com/huawei/devcloud/monitor/utils/IamJudge.java
@@ -11,25 +11,6 @@ public class IamJudge {
 	private String encodeUserName;
 	private String encodePwd;
 	
-	public static final String TENANT_BODY = 
-			"{" +
-			"	    \"auth\": {" +
-			"	        \"identity\": {" +
-			"	            \"methods\": [" +
-			"	                \"hw_assume_role\"" +
-			"	            ]," +
-			"	            \"hw_assume_role\": {" +
-			"	                \"domain_id\": \"%s\"," +
-			"	                \"xrole_name\": \"op_service\"" +
-			"	            }" +
-			"	        }," +
-			"	        \"scope\": {" +
-			"	            \"domain\": {" +
-			"	                \"id\": \"%s\"" +
-			"	            }" +
-			"	        }" +
-			"	    }" +
-			"	}" ;
 	
 	public IamJudge(String iamUrl, String encodeUserName, String encodePwd) {
 		this.iamUrl = iamUrl;
@@ -50,28 +31,6 @@ public class IamJudge {
 			logger.log(Level.WARNING,"[build-notification] getCommonToken token is empty");
 		}
 		return token;
-	}
-	
-	public String loadTenantToken(String domanId) {
-		try {
-			String pkiToken = getCommonToken();
-			String certServerUrl = this.iamUrl + "/v3/auth/tokens";
-			if (checkEmpty(new String[] { domanId, pkiToken, iamUrl })) {
-				logger.log(Level.WARNING,"[build-notification] null para for token request.");
-			    return null;
-	    	}
-		    String strBody = String.format(TENANT_BODY, 
-		    		domanId, domanId);
-		    ReqestClientUtil requestUtil = new ReqestClientUtil();
-		    String token = requestUtil.loadIAMToken(certServerUrl, strBody, pkiToken);
-			if (CommonUtils.isNullOrEmpty(token)) {
-				logger.log(Level.WARNING,"[build-notification] token isEmpty.");
-			}
-			return token;
-		} catch (AuthException e) {
-			logger.log(Level.SEVERE,"[build-notification] invalid common username and pwd.");
-		}
-		return null;
 	}
 	
 	
